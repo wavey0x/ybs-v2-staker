aceec47e8f2b961f8ca0fa981b70ea0f9165b9f5
dudesahn <dudesahn@pm.me>
2024-10-22 16:22:07 -0500

    chore: review
    * remove extra state vars and modifier
    * lint w/ prettier and black

--- contracts/SwapperV3.sol
+++ contracts/SwapperV3.sol
@@ -6,12 +6,17 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 import {ICurve} from "./interfaces/curve/ICurve.sol";
 import {ICurveInt128} from "./interfaces/curve/ICurveInt128.sol";
 
-
 interface IZap {
-    function zap(address _inputToken, address _outputToken, uint256 _amountIn, uint256 _minOut, address _recipient) external returns (uint256);
+    function zap(
+        address _inputToken,
+        address _outputToken,
+        uint256 _amountIn,
+        uint256 _minOut,
+        address _recipient
+    ) external returns (uint256);
 }
 
-interface IVault{
+interface IVault {
     struct StrategyParams {
         uint256 performanceFee;
         uint256 activation;
@@ -23,8 +28,11 @@ interface IVault{
         uint256 totalGain;
         uint256 totalLoss;
     }
+
     function deposit(uint amount, address recipient) external returns (uint);
+
     function strategies(address) external returns (StrategyParams memory);
+
     function asset() external returns (address);
 }
 
@@ -43,16 +51,19 @@ contract SwapperV3 {
     int128 public pool2OutTokenIdx;
     bool otcDisabled;
     address public constant owner = 0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52;
-    address public constant treasury = 0x93A62dA5a14C80f265DAbC077fCEE437B1a0Efde;
+    address public constant treasury =
+        0x93A62dA5a14C80f265DAbC077fCEE437B1a0Efde;
     IZap public constant zap = IZap(0x78ada385b15D89a9B845D2Cac0698663F0c69e3C);
     IVault public vault = IVault(0xBF319dDC2Edc1Eb6FDf9910E39b37Be221C8805F);
-    IVault public approvedVault = IVault(0x27B5739e22ad9033bcBf192059122d163b60349D);
+    IVault public approvedVault =
+        IVault(0x27B5739e22ad9033bcBf192059122d163b60349D);
     mapping(address => bool) public allowed;
 
     modifier isAllowed() {
         require(
-            approvedVault.strategies(msg.sender).activation > 0 || 
-            allowed[msg.sender], "!Allowed"
+            approvedVault.strategies(msg.sender).activation > 0 ||
+                allowed[msg.sender],
+            "!Allowed"
         );
         _;
     }
@@ -72,21 +83,21 @@ contract SwapperV3 {
         pool1 = _pool1;
         pool2 = _pool2;
         tokenOutPool1 = _tokenOutPool1;
-        
+
         uint idxFound;
         address token;
 
-        for(uint i; i < 3; ++i){
+        for (uint i; i < 3; ++i) {
             token = _pool1.coins(i);
-            if(token == address(_tokenIn)) {
+            if (token == address(_tokenIn)) {
                 pool1InTokenIdx = i;
                 idxFound++;
-                if(idxFound == 2) break;
+                if (idxFound == 2) break;
             }
-            if(token == address(_tokenOutPool1)) {
+            if (token == address(_tokenOutPool1)) {
                 pool1OutTokenIdx = i;
                 idxFound++;
-                if(idxFound == 2) break;
+                if (idxFound == 2) break;
             }
         }
 
@@ -99,21 +110,35 @@ contract SwapperV3 {
         tokenIn.safeTransferFrom(msg.sender, address(this), _amount);
         if (!otcDisabled) (profit, _amount) = _sellOtc(_amount);
         if (_amount < PRECISION) return profit;
-        uint out = pool1.exchange_underlying(pool1InTokenIdx, pool1OutTokenIdx, _amount, 0);
-        return profit += zap.zap(address(tokenOutPool1), address(tokenOut), out, 0, msg.sender);
+        uint out = pool1.exchange_underlying(
+            pool1InTokenIdx,
+            pool1OutTokenIdx,
+            _amount,
+            0
+        );
+        return
+            profit += zap.zap(
+                address(tokenOutPool1),
+                address(tokenOut),
+                out,
+                0,
+                msg.sender
+            );
     }
 
     // Returns amount of profit and amount of sell tokens remaining to be sold.
-    function _sellOtc(uint _sellTokenAmount) internal isAllowed returns (uint, uint) {
+    function _sellOtc(
+        uint _sellTokenAmount
+    ) internal isAllowed returns (uint, uint) {
         ERC20 buyToken = tokenOut;
         uint buyTokenBalance = buyToken.balanceOf(address(this));
         if (buyTokenBalance < PRECISION) return (0, _sellTokenAmount);
         uint price = priceOracle();
         uint amountToSell = _sellTokenAmount;
-        uint amountToBuy = amountToSell * price / PRECISION;
+        uint amountToBuy = (amountToSell * price) / PRECISION;
         if (amountToBuy > buyTokenBalance) {
             amountToBuy = buyTokenBalance;
-            amountToSell = PRECISION * buyTokenBalance / price;
+            amountToSell = (PRECISION * buyTokenBalance) / price;
         }
         buyToken.safeTransfer(msg.sender, amountToBuy);
         vault.deposit(amountToSell, treasury);
@@ -126,7 +151,7 @@ contract SwapperV3 {
     function priceOracle() public view returns (uint) {
         uint oraclePricePool1 = pool1.price_oracle(1); // 1 = CRV index
         uint oraclePricePool2 = pool2.price_oracle();
-        return PRECISION * oraclePricePool2 / oraclePricePool1;
+        return (PRECISION * oraclePricePool2) / oraclePricePool1;
     }
 
     function sweep(address _token) external {
@@ -153,5 +178,4 @@ contract SwapperV3 {
         allowed[_caller] = _isAllowed;
         emit SetAllowed(_caller, _isAllowed);
     }
-
 }
--- contracts/SwapperV4.sol
+++ contracts/SwapperV4.sol
@@ -6,12 +6,17 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 import {ICurve} from "./interfaces/curve/ICurve.sol";
 import {ICurveInt128} from "./interfaces/curve/ICurveInt128.sol";
 
-
 interface IZap {
-    function zap(address _inputToken, address _outputToken, uint256 _amountIn, uint256 _minOut, address _recipient) external returns (uint256);
+    function zap(
+        address _inputToken,
+        address _outputToken,
+        uint256 _amountIn,
+        uint256 _minOut,
+        address _recipient
+    ) external returns (uint256);
 }
 
-interface IVault{
+interface IVault {
     struct StrategyParams {
         uint256 performanceFee;
         uint256 activation;
@@ -23,8 +28,11 @@ interface IVault{
         uint256 totalGain;
         uint256 totalLoss;
     }
+
     function deposit(uint amount, address recipient) external returns (uint);
+
     function strategies(address) external returns (StrategyParams memory);
+
     function asset() external returns (address);
 }
 
@@ -39,27 +47,23 @@ contract SwapperV4 {
     ICurveInt128 public immutable pool2;
     uint public pool1InTokenIdx;
     uint public pool1OutTokenIdx;
-    int128 public pool2InTokenIdx;
-    int128 public pool2OutTokenIdx;
     bool public otcEnabled;
     address public constant owner = 0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52;
-    address public constant treasury = 0x93A62dA5a14C80f265DAbC077fCEE437B1a0Efde;
+    address public constant treasury =
+        0x93A62dA5a14C80f265DAbC077fCEE437B1a0Efde;
     IZap public constant zap = IZap(0x78ada385b15D89a9B845D2Cac0698663F0c69e3C);
     IVault public vault = IVault(0xBF319dDC2Edc1Eb6FDf9910E39b37Be221C8805F);
-    IVault public approvedVault = IVault(0x27B5739e22ad9033bcBf192059122d163b60349D);
+    IVault public approvedVault =
+        IVault(0x27B5739e22ad9033bcBf192059122d163b60349D);
     address public management;
     mapping(address => bool) public allowedSwapper;
 
     modifier isAllowedSwapper() {
         require(
-            approvedVault.strategies(msg.sender).activation > 0 || 
-            allowedSwapper[msg.sender], "!AllowedSwapper"
+            approvedVault.strategies(msg.sender).activation > 0 ||
+                allowedSwapper[msg.sender],
+            "!AllowedSwapper"
         );
-        _;
-    }
-
-    modifier onlyManagement() {
-        require(msg.sender == management, "!management");
         _;
     }
 
@@ -69,7 +73,10 @@ contract SwapperV4 {
     }
 
     modifier onlyOwnerOrManagement() {
-        require(msg.sender == owner || msg.sender == management, "!ownerOrManagement");
+        require(
+            msg.sender == owner || msg.sender == management,
+            "!ownerOrManagement"
+        );
         _;
     }
 
@@ -91,21 +98,21 @@ contract SwapperV4 {
         pool1 = _pool1;
         pool2 = _pool2;
         tokenOutPool1 = _tokenOutPool1;
-        
+
         uint idxFound;
         address token;
 
-        for(uint i; i < 3; ++i){
+        for (uint i; i < 3; ++i) {
             token = _pool1.coins(i);
-            if(token == address(_tokenIn)) {
+            if (token == address(_tokenIn)) {
                 pool1InTokenIdx = i;
                 idxFound++;
-                if(idxFound == 2) break;
+                if (idxFound == 2) break;
             }
-            if(token == address(_tokenOutPool1)) {
+            if (token == address(_tokenOutPool1)) {
                 pool1OutTokenIdx = i;
                 idxFound++;
-                if(idxFound == 2) break;
+                if (idxFound == 2) break;
             }
         }
 
@@ -118,21 +125,35 @@ contract SwapperV4 {
         tokenIn.safeTransferFrom(msg.sender, address(this), _amount);
         if (otcEnabled) (profit, _amount) = _sellOtc(_amount);
         if (_amount < PRECISION) return profit;
-        uint out = pool1.exchange_underlying(pool1InTokenIdx, pool1OutTokenIdx, _amount, 0);
-        return profit += zap.zap(address(tokenOutPool1), address(tokenOut), out, 0, msg.sender);
+        uint out = pool1.exchange_underlying(
+            pool1InTokenIdx,
+            pool1OutTokenIdx,
+            _amount,
+            0
+        );
+        return
+            profit += zap.zap(
+                address(tokenOutPool1),
+                address(tokenOut),
+                out,
+                0,
+                msg.sender
+            );
     }
 
     // Returns amount of profit and amount of sell tokens remaining to be sold.
-    function _sellOtc(uint _sellTokenAmount) internal isAllowedSwapper returns (uint, uint) {
+    function _sellOtc(
+        uint _sellTokenAmount
+    ) internal isAllowedSwapper returns (uint, uint) {
         ERC20 buyToken = tokenOut;
         uint buyTokenBalance = buyToken.balanceOf(address(this));
         if (buyTokenBalance < PRECISION) return (0, _sellTokenAmount);
         uint price = priceOracle();
         uint amountToSell = _sellTokenAmount;
-        uint amountToBuy = amountToSell * price / PRECISION;
+        uint amountToBuy = (amountToSell * price) / PRECISION;
         if (amountToBuy > buyTokenBalance) {
             amountToBuy = buyTokenBalance;
-            amountToSell = PRECISION * buyTokenBalance / price;
+            amountToSell = (PRECISION * buyTokenBalance) / price;
         }
         buyToken.safeTransfer(msg.sender, amountToBuy);
         vault.deposit(amountToSell, treasury);
@@ -145,7 +166,7 @@ contract SwapperV4 {
     function priceOracle() public view returns (uint) {
         uint oraclePricePool1 = pool1.price_oracle(1); // 1 = CRV index
         uint oraclePricePool2 = pool2.price_oracle();
-        return PRECISION * oraclePricePool2 / oraclePricePool1;
+        return (PRECISION * oraclePricePool2) / oraclePricePool1;
     }
 
     function sweep(address _token) external onlyOwnerOrManagement {
@@ -167,7 +188,10 @@ contract SwapperV4 {
     }
 
     // Permit a caller to OTC against funds in this contract
-    function setAllowedSwapper(address _caller, bool _isAllowed) external onlyOwnerOrManagement {
+    function setAllowedSwapper(
+        address _caller,
+        bool _isAllowed
+    ) external onlyOwnerOrManagement {
         allowedSwapper[_caller] = _isAllowed;
         emit SetAllowedSwapper(_caller, _isAllowed);
     }
